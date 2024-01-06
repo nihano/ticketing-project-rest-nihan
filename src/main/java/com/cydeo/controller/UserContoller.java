76799ccc77cd28fa558964d7a23ed444f44bb5bc
nihano <nioztekin@gmail.com>
2024-01-06 16:11:19 +0300

    roles added

--- src/main/java/com/cydeo/controller/UserContoller.java
+++ src/main/java/com/cydeo/controller/UserContoller.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -19,32 +20,38 @@ public class UserContoller {
         this.userService = userService;
     }
 
+
     @GetMapping
-    public ResponseEntity<ResponseWrapper> getUsers(){
+    @RolesAllowed({"Manager", "Admin"})
+    public ResponseEntity<ResponseWrapper> getUsers() {
         List<UserDTO> userDTOList = userService.listAllUsers();
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved", userDTOList, HttpStatus.OK));
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String userName){
+    @RolesAllowed({"Admin"})
+    public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String userName) {
         UserDTO userDTO = userService.findByUserName(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved", userDTO, HttpStatus.OK));
     }
 
     @PostMapping
-    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
+    @RolesAllowed({"Admin"})
+    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user) {
         userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created", HttpStatus.CREATED));
     }
 
     @PutMapping
-    public ResponseEntity<ResponseWrapper> updateUser(UserDTO user){
+    @RolesAllowed({"Admin"})
+    public ResponseEntity<ResponseWrapper> updateUser(UserDTO user) {
         userService.update(user);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully updated", HttpStatus.OK));
     }
 
     @DeleteMapping("/{username}")
-    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String userName){
+    @RolesAllowed({"Admin"})
+    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String userName) {
         userService.delete(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted", HttpStatus.OK));
     }
